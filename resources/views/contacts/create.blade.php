36a7d4214d392c88774ee4d036eff33e294790ba
Hartiel <hartielag@gmail.com>
2025-06-19 15:20:44 -0300

    Fix: Add error flashes in create pages

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -17,16 +17,25 @@
         <div>
             <label for="name">Nome:</label><br>
             <input type="text" id="name" name="name" required>
+            @error('name')
+                <div style="color: red; font-size: 0.9em;">{{ $message }}</div>
+            @enderror
         </div>
         <br>
         <div>
             <label for="contact">Contato (9 dígitos):</label><br>
             <input type="text" id="contact" name="contact" required>
+            @error('contact')
+                <div style="color: red; font-size: 0.9em;">{{ $message }}</div>
+            @enderror
         </div>
         <br>
         <div>
             <label for="email">Email:</label><br>
             <input type="email" id="email" name="email" required>
+            @error('email')
+                <div style="color: red; font-size: 0.9em;">{{ $message }}</div>
+            @enderror
         </div>
         <br>
         <button type="submit">Salvar Contato</button>
